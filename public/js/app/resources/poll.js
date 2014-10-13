dd7d0eead7aefb7864c28a289fe7823bb96d9cd6
Alexandre Magno Alves Pereira <alexmagno95@97.238.69.111.dynamic.snap.net.nz>
2014-10-14 12:54:10 +1300

    changing path for mongodb

--- public/js/app/resources/poll.js
+++ public/js/app/resources/poll.js
@@ -1,14 +1,14 @@
 polls.factory("Poll", ["$resource", function($resource) {
 
-  return $resource("http://agile-depths-4725.herokuapp.com/polls:"+process.env.PORT+"/:id", {}, {
+  return $resource("http://agile-depths-4725.herokuapp.com/polls:/:id", {}, {
     getAll: {
       method: "GET",
       isArray: true,
-      url: "http://agile-depths-4725.herokuapp.com:"+process.env.PORT+"/polls"
+      url: "http://agile-depths-4725.herokuapp.com:/polls"
     },
     get: {
       method: "GET",
-      url: "http://agile-depths-4725.herokuapp.com:"+process.env.PORT+"/polls/:id"
+      url: "http://agile-depths-4725.herokuapp.com:/polls/:id"
     },
   });
 
